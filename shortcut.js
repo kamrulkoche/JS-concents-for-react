0050f33e797d968bab295b070a72948bf6fcb952
kamrulkoche <kamrulkoche@gmail.com>
2024-05-05 12:25:16 +0600

    shortcut

---
+++ shortcut.js
@@ -0,0 +1,54 @@
+//'almas',5,true,{},[]
+//'',0,false ,null.undefined
+//check truthy
+let myVar = 5;
+//check any truthy
+if (myVar) {
+  myVar = myVar * 100;
+} else {
+  myVar = 0;
+}
+
+let muMoney = 50;
+//you check negative or falsy anything
+if (!muMoney) {
+}
+
+const money = 101;
+let food;
+if (money > 100) {
+  food = "biryani";
+} else {
+  food = "cha biscuit";
+}
+
+let food1 = money > 100 ? "biriyani" : "cha biscuit";
+// console.log(food1);
+
+let drink = money > 100 && myVar > 100 ? "coke" : "filter water";
+// console.log(drink);
+
+//number to string conversion
+const num1 = 52;
+// console.log(num1);
+const numStr = num1 + "";
+// console.log(numStr);
+// console.log(typeof(numStr));
+
+//string to number
+const input = "560";
+const inputNum = +input;
+console.log(inputNum);
+
+//
+let isActive = true;
+const showUser = () => console.log("display User");
+const hideUser = () => console.log("hide User");
+!isActive ? showUser() : hideUser();
+//use && if the left side is true then right side wil be executed
+isActive && showUser();
+//use || if the left side is false then right side will be executed
+isActive || hideUser();
+
+//toggle boolean
+isActive = !isActive;
